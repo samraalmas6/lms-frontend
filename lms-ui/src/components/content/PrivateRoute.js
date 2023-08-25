528bf60d7898349e281451b45c3812acdb5e7828
Mohsen Ali <mohsen.ali@xloopdigital.com>
2023-08-25 09:15:27 +0500

    Working on Verification page

--- lms-ui/src/components/content/PrivateRoute.js
+++ lms-ui/src/components/content/PrivateRoute.js
@@ -12,10 +12,10 @@ export default function PrivateRoute({ children }) {
     <div>
       <div className="container-fluid" id="main">
         <div className="row row-offcanvas row-offcanvas-left">
-          <div className="col-sm-4 col-md-3 col-xl-2 px-sm-2 px-0 pe-0">
+          <div className="col-xxl-2 col-xl-3 col-lg-3 col-md-4 col-sm-4 col-xs-12 px-sm-2 col-lg-4 px-0 pe-0">
             <Sidebar />
           </div>
-          <div className="col-sm-8 col-md-9 col-xl-10 ps-1">
+          <div className="col-xxl-10 col-xl-9 col-lg-8 col-md-7 col-xs-12 col-sm-6 ps-1">
             <Navbar />
             <div className="col pt-2 mt-2 main-body" style={{minHeight: '85vh'}}>{children}</div>
           </div>
