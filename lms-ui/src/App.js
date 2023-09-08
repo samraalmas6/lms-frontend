734a0cf8b98ce0efdb230a73294e99e25d5fc049
Samara Mohsin <Samara Mohsin@DESKTOP-BP4905N>
2023-09-08 12:00:40 +0500

    tabs switching div in progress

--- lms-ui/src/App.js
+++ lms-ui/src/App.js
@@ -1,49 +1,103 @@
 import "./components/styles/App.css";
-import "./components/styles/HomePage.css"
-import "./components/styles/Responsive.css"
+import "./components/styles/HomePage.css";
+import "./components/styles/Responsive.css";
 
 import SigninPage from "./components/pages/SigninPage";
-import HomePage from './components/content/HomePage'
-import CourseTable from './components/pages/CourseTable'
+import HomePage from "./components/content/HomePage";
+import CourseTable from "./components/pages/CourseTable";
 import { useState } from "react";
-import {
-	BrowserRouter,
-	Routes,
-	Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import AddUser from "./components/pages/AddUser";
-import PrivateRoute from "./components/content/PrivateRoute"
+import PrivateRoute from "./components/content/PrivateRoute";
 import AllUsers from "./components/pages/AllUsers";
 import CreateCourse from "./components/pages/courses/CreateCourse";
 import AllCourse from "./components/pages/courses/AllCourses";
 import VerificationPage from "./components/pages/VerificationPage";
+import MyCourses from "./components/pages/courses/MyCourses";
 
 function App() {
-  const [showlogin, setShowLogin] = useState(false)
+  const [showlogin, setShowLogin] = useState(false);
 
   return (
     <div>
       <CourseTable />
-       {/* <BrowserRouter>
-       <Routes>
-       <Route element={<SigninPage />} path="/auth/login" />
-       <Route element= {<VerificationPage />} path="/auth/verification"></Route>
-       </Routes>
-			<Routes>
-				<Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>}></Route>
-        <Route path="/dashboard" element={<PrivateRoute><HomePage /></PrivateRoute>} ></Route>
-				<Route path="/home" element={<PrivateRoute><HomePage /></PrivateRoute>} ></Route>
-        <Route path="/adduser" element={<PrivateRoute><AddUser /></PrivateRoute>}></Route>
-        <Route path="/allusers" element={<PrivateRoute><AllUsers /></PrivateRoute>}></Route>
-        <Route path="/course/create" element={<PrivateRoute><CreateCourse /></PrivateRoute>}></Route>
-        <Route path="/course/all" element={<PrivateRoute><AllCourse /></PrivateRoute>}></Route>
-        <Route path="/course/my-courses" element={<PrivateRoute><MyCourses /></PrivateRoute>}></Route>
-      </Routes>
-
+      {/* <BrowserRouter>
+        <Routes>
+          <Route element={<SigninPage />} path="/auth/login" />
+          <Route
+            element={<VerificationPage />}
+            path="/auth/verification"
+          ></Route>
+        </Routes>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/adduser"
+            element={
+              <PrivateRoute>
+                <AddUser />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/allusers"
+            element={
+              <PrivateRoute>
+                <AllUsers />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/course/create"
+            element={
+              <PrivateRoute>
+                <CreateCourse />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/course/all"
+            element={
+              <PrivateRoute>
+                <AllCourse />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/course/my-courses"
+            element={
+              <PrivateRoute>
+                <MyCourses />
+              </PrivateRoute>
+            }
+          ></Route>
+        </Routes>
       </BrowserRouter> */}
-
-  </div>
+    </div>
   );
 }
 
--- lms-ui/src/components/pages/ModuleCard.js
+++ lms-ui/src/components/pages/ModuleCard.js
@@ -2,103 +2,122 @@ import React, { useState } from "react";
 import YouTube from "react-youtube";
 import Collapse from "react-collapse";
 import styles from "../styles/CourseTable.module.css";
-import ReactPlayer from 'react-player';
+import ReactPlayer from "react-player";
 import "../styles/CourseTable.css";
 
 const ModuleCard = ({ module }) => {
   const [selectedLesson, setSelectedLesson] = useState(null);
+  const [selectedModule, setSelectedModule] = useState(null);
   const [isModuleExpanded, setIsModuleExpanded] = useState(false);
 
   const selectLesson = (lesson) => {
     setSelectedLesson(lesson);
   };
 
+  const selectModule = ({ module }) => {
+    setSelectedModule(module);
+  };
+
   const toggleModuleExpand = () => {
     setIsModuleExpanded(!isModuleExpanded);
   };
+
 
   const videoUrl = "https://youtu.be/apGV9Kg7ics?si=yP2oeVUi684WxZyg";
 
   // console.log(selectedLesson)
   // console.log(selectedLesson && selectedLesson.url)
   return (
-    <div className="module-card">
-      <div className="module-header" onClick={toggleModuleExpand}>
-        <h3>{module.title}</h3>
-        <i class="fas fa-caret-up fa-rotate-180 arrow"></i>
-      </div>
-      <Collapse isOpened={isModuleExpanded}>
-        <div className="module-list">
-          <ul className="module-content">
-            {module.lessons.map((lesson) => (
-              <>
-                <div className="module-content-container">
-                  <div className="check-box-div">
-                    {/* <Checkbox {...label} /> */}
-                    <form action="">
-                      <input
-                        className="checkbox"
-                        type="checkbox"
-                        id="lesson"
-                        name="lesson"
-                        value="lesson"
-                      />
-                    </form>
+    <>
+      <div className="module-card">
+        <div className="module-header" onClick={toggleModuleExpand}>
+          <h3>{module.title}</h3>
+          <i class="fas fa-caret-up fa-rotate-180 arrow"></i>
+        </div>
+        <Collapse isOpened={isModuleExpanded}>
+          <div className="module-list">
+            <ul className="module-content">
+              {module.lessons.map((lesson) => (
+                <>
+                  <div className="module-content-container">
+                    <div className="check-box-div">
+                      <form action="">
+                        <input
+                          className="checkbox"
+                          type="checkbox"
+                          id="lesson"
+                          name="lesson"
+                          value="lesson"
+                        />
+                      </form>
+                    </div>
+                    <div className="content-inside-container">
+                      <li
+                        key={lesson.id}
+                        onClick={() => selectLesson(lesson)}
+                        className={`lesson-item ${
+                          selectedLesson === lesson ? "active" : ""
+                        } upper-row`}
+                      >
+                        <li>{lesson.sno}</li>
+                        <li> {lesson.title}</li>
+                      </li>
+                      <li>
+                        <div className="video-player-icon">
+                          <i class="fas fa-solid fa-tv"></i>
+                          {lesson.duration}
+                        </div>
+                      </li>
+                    </div>
                   </div>
-                  <div className="content-inside-container">
+                </>
+              ))}
+            </ul>
+          </div>
+        </Collapse>
+        <div className="video_player_container">
+          {selectedLesson && selectedLesson.url && (
+            <div className="video-player-container">
+              {console.log(selectedLesson.url)}
+              <ReactPlayer
+                url={selectedLesson.url}
+                controls={true}
+                width="100%"
+              />
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* chatgpt */}
+      {/* <div className="module-card">
+        <div className="module-list">
+          <h3 onClick={toggleModuleExpand}>
+            {selectedModule ? selectedModule.title : "Select a Module"}
+          </h3>
+          <Collapse isOpened={isModuleExpanded}>
+            {modules.map((module) => (
+              <div key={module.id} className="module-item">
+                <h3 onClick={() => selectModule(module)}>{module.title}</h3>
+                <ul>
+                  {module.lessons.map((lesson) => (
                     <li
                       key={lesson.id}
                       onClick={() => selectLesson(lesson)}
                       className={`lesson-item ${
                         selectedLesson === lesson ? "active" : ""
-                      } upper-row`}
+                      }`}
                     >
-                      <li>{lesson.sno}</li>
-                      <li> {lesson.title}</li>
+                      {lesson.title}
                     </li>
-                    <li>
-                      <div className="video-player-icon">
-                        <i class="fas fa-solid fa-tv"></i>
-                        {lesson.duration}
-                      </div>
-                    </li>
-                    {/* <i class="fas fa-solid fa-play"></i> */}
-                  </div>
-                </div>
-              </>
+                  ))}
+                </ul>
+              </div>
             ))}
-          </ul>
+          </Collapse>
         </div>
-
-        {/* -------------xxxxxxxxxxxxxx---------------- */}
-        {/* <div className="module-content">
-          <div className="row-1">
-            {module.lessons.map((lesson) => (
-                 key={lesson.id}
-              onClick = {() =>selectLesson(lesson)}
-              className={`lesson-item ${selectedLesson === lesson ? 'active' : ''}`}
-            ))}
-          </div>
-          <div className="row-2"></div>
-        </div> */}
-      </Collapse>
-      <div className="video_player_container">
-        {selectedLesson && selectedLesson.url && (
-          // <YouTube videoId={selectedLesson.videoId} />
-          <div className="video-player-container">
-            {/* {module.lessons.map((lesson) => ( */}
-              {console.log(selectedLesson.url)}
-               <ReactPlayer
-               url={selectedLesson.url}
-               controls={true} // Show video controls (play, pause, volume, etc.)
-               width="100%"
-              //  height="100%"
-             />
-             {/* ))} */}
-          </div>
-        )}
-      </div>
-    </div>
+      </div> */}
+    </>
   );
 };
 
