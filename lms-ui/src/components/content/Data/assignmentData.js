cc016fd38b9609e539966c769635f86565aeb963
exit <abeeraarshad2019@gmail.com>
2023-10-01 03:31:01 +0500

    assignment table screen completed

--- lms-ui/src/components/content/Data/assignmentData.js
+++ lms-ui/src/components/content/Data/assignmentData.js
@@ -3,28 +3,42 @@ const assignmentData = [
       id: 1,
       submitted_by: "hammad",
       assignment: "Assign1",
-      content: "pdf file.pdf",
+      content: "http://www.codewithharry.com",
       submission_date: "28-09-2023",
       Due_Date: "30-09-2023",
-      Graders_Name: "Samara"
+      Graders_Name: "Samara",
+      Partners: "Mohsin",
+      Grade: "50"
+    
+      
+
     },
     {
       id: 2,
       submitted_by: "Abeera",
       assignment: "Assign1",
-      content: "pdf file.pdf",
+      content:  "http://www.codewithharry.com",
       submission_date: "28-09-2023",
-      Due_Date: "30-09-2023"
+      Due_Date: "30-09-2023",
+      Graders_Name: "Samara",
+      Partners: "Mohsin",
+      Grade: "70"
     },
     {
       id: 3,
       submitted_by: "Abeera",
       assignment: "Assign1",
-      content: "pdf file.pdf",
+      content: "http://www.codewithharry.com",
       submission_date: "28-09-2023",
-      Due_Date: "30-09-2023"
+      Due_Date: "30-09-2023",
+      Graders_Name: "Samara",
+      Partners: "Mohsin",
+      Grade: "90"
     }
+    
   ];
+
+ 
   
   export default assignmentData;
   
