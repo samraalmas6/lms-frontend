9af751f1d79ee96f70ff54799664287d98d3f39a
exit <abeeraarshad2019@gmail.com>
2023-10-01 14:26:42 +0500

    assignment mock data changes

--- lms-ui/src/components/content/Data/assignmentData.js
+++ lms-ui/src/components/content/Data/assignmentData.js
@@ -1,79 +1,81 @@
 const assignmentData = [
-    {
-      id: 1,
-      submitted_by: "hammad",
-      assignment: "Assign1",
-      content: "http://www.codewithharry.com",
-      submission_date: "28-09-2023",
-      Due_Date: "30-09-2023",
-      Graders_Name: "Samara",
-      Partners: "Mohsin",
-      Grade: "50"
-    
-      
+  {
+    id: 1,
+    submitted_by: "hammad",
+    assignment: "Assign1",
+    content: "http://www.codewithharry.com",
+    submission_date: "28-09-2023",
+    Due_Date: "30-09-2023",
+    Graders_Name: "Samara",
+    Partners: "Mohsin",
+    Grade: "50",
+  },
+  {
+    id: 2,
+    submitted_by: "Abeera",
+    assignment: "Assign1",
+    content: "http://www.codewithharry.com",
+    submission_date: "28-09-2023",
+    Due_Date: "30-09-2023",
+    Graders_Name: "Samara",
+    Partners: "Mohsin",
+    Grade: "70",
+  },
+  {
+    id: 3,
+    submitted_by: "hammad",
+    assignment: "Assign1",
+    content: "http://www.codewithharry.com",
+    submission_date: "28-09-2023",
+    Due_Date: "30-09-2023",
+    Graders_Name: "Samara",
+    Partners: "Mohsin",
+    Grade: "50",
+  },
+  {
+    id: 4,
+    submitted_by: "Abeera",
+    assignment: "Assign1",
+    content: "http://www.codewithharry.com",
+    submission_date: "28-09-2023",
+    Due_Date: "30-09-2023",
+    Graders_Name: "Samara",
+    Partners: "Mohsin",
+    Grade: "70",
+  },
+  {
+    id: 5,
+    submitted_by: "hammad",
+    assignment: "Assign1",
+    content: "http://www.codewithharry.com",
+    submission_date: "28-09-2023",
+    Due_Date: "30-09-2023",
+    Graders_Name: "Samara",
+    Partners: "Mohsin",
+    Grade: "50",
+  },
+  {
+    id: 6,
+    submitted_by: "Abeera",
+    assignment: "Assign1",
+    content: "http://www.codewithharry.com",
+    submission_date: "28-09-2023",
+    Due_Date: "30-09-2023",
+    Graders_Name: "Samara",
+    Partners: "Mohsin",
+    Grade: "70",
+  },
+  {
+    id: 7,
+    submitted_by: "XYZ",
+    assignment: "Assign2",
+    content: "http://www.codewithharry.com",
+    submission_date: "28-09-2023",
+    Due_Date: "30-09-2023",
+    Graders_Name: "Samara",
+    Partners: "Mohsin",
+    Grade: "90",
+  },
+];
 
-    },
-    {
-      id: 2,
-      submitted_by: "Abeera",
-      assignment: "Assign1",
-      content:  "http://www.codewithharry.com",
-      submission_date: "28-09-2023",
-      Due_Date: "30-09-2023",
-      Graders_Name: "Samara",
-      Partners: "Mohsin",
-      Grade: "70"
-    },
-    {
-      id: 3,
-      submitted_by: "Samra",
-      assignment: "Assign3",
-      content: "pdf file.pdf",
-      submission_date: "28-09-2023",
-    },
-    {
-      id: 4,
-      submitted_by: "Mohsen",
-      assignment: "Assign2",
-      content: "pdf file.pdf",
-      submission_date: "28-09-2023",
-    },
-    {
-      id: 5,
-      submitted_by: "Hammad",
-      assignment: "Assign2",
-      content: "pdf file.pdf",
-      submission_date: "28-09-2023",
-    },
-    {
-      id: 6,
-      submitted_by: "Humza",
-      assignment: "Assign2",
-      content: "pdf file.pdf",
-      submission_date: "28-09-2023",
-    },
-    {
-      id: 7,
-      submitted_by: "Mudassir",
-      assignment: "Assign2",
-      content: "pdf file.pdf",
-      submission_date: "28-09-2023",
-    },
-    {
-      id: 8,
-      submitted_by: "XYZ",
-      assignment: "Assign2",
-      content: "http://www.codewithharry.com",
-      submission_date: "28-09-2023",
-      Due_Date: "30-09-2023",
-      Graders_Name: "Samara",
-      Partners: "Mohsin",
-      Grade: "90"
-    }
-    
-  ];
-
- 
-  
-  export default assignmentData;
-  
+export default assignmentData;
